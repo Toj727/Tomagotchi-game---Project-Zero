240ab850f60386b0a1d5adefe60c7fe0606bc713
AMITOJ SINGH <singhamitoj10@gmail.com>
2021-07-13 14:42:13 -0400

    base game plan

---
+++ scripts/apps.js
@@ -0,0 +1,20 @@
+console.log("Don't Kill The Cat");
+
+/* == MVP == */
+
+// 20+ commits
+// Working game
+
+// User story names their pet and clicks start the game begins and new content box pops up.
+// Content box has 3 buttons - Feed, Pet, and a Light toggle.
+// Feed decreases hunger variable
+// Pet decreases Boredom variable
+// light toggle decreases sleepiness variable
+// All variables (except age) start at 5 and increase every 30s
+// Age starts at 1 and increases every minute by 1.
+// At Ages 3, 6, and 9, cat grows bigger 
+
+// All variables increase as time increment increases between corresponding button clicks. Except age increases continually unless game ends.
+
+// Game ends when any variable reaches 10.
+
